8f9bed1983239fa5f9e5dad4c49b03b4307b26b5
Satyendra3335 <satyendrasinghfzd184@gmail.com>
2024-03-04 09:46:18 +0530

    My First project

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -10,7 +10,7 @@ const Card = () => {
         <div className='card_container'>
                 <div><img src={cardOne} alt="card-img"/> </div>
                 <h1>Waste pickers</h1>
-                <span>by Satyendra Singh</span>
+                <span>by Satyendra Singh </span>
                 <p>Every struggles of those who keeps our street clean.
                 If we look at the life of...</p>
                 <button>Read More</button>
